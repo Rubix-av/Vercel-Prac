c33390e7f3cda9c89556dc325aac6745acc1d388
Rubix-av <130751726+Rubix-av@users.noreply.github.com>
2025-05-21 17:31:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
 from fastapi import FastAPI, Query
 from typing import List
+import json
 
 app = FastAPI()
 
 @app.get("/")
 def root(name: List[str] = Query(...)):
-  name1, name2 = name[0], name[1]
-  return {"name1": name1, "name2": name2}
+  
+  name1, name2 = name[0].strip('"'), name[1].strip('"')
+  
+  marks_list = []
+  
+  with open("q-vercel-python.json", "r") as f:
+    data = json.load(f)  
+  
+  flag = False
+
+  for details in data:
+    if details["name"] == name1:
+      marks_list.append(details["marks"])
+      flag = True
+    
+    if flag == True and details["name"] == name2:
+      marks_list.append(details["marks"])
+    
+  return {"marks": marks_list}
